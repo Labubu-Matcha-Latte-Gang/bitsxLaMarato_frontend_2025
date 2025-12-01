1c726ae84beec8e5dee662472975dc41eba344e6
kaleb.grove <72881364+kalebgrove@users.noreply.github.com>
2025-12-01 19:05:58 +0100

    Add helper functions and constants for icons and images; update login screen layout

---
+++ lib/utils/constants/icon_strings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/src/widgets/icon_data.dart';
+
+/// Class for the icon string constants
+
+class TIcons {
+  static const String eyeOpen = "assets/icons/open-eye.png";
+  static const String eyeClosed = "assets/icons/closed-eye.png";
+  static const String email = "assets/icons/email-icon.png";
+
+}
