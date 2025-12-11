bde20541aa05dc2ec9539b4c260bc013e750acda
oorbea <oriol.orbea@gmail.com>
2025-12-11 17:59:25 +0100

    Add doctor home page navigation and user type handling in main app logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:bitsxlamarato_frontend_2025/features/screens/register/registerLo
 import 'package:bitsxlamarato_frontend_2025/features/screens/login/login.dart';
 import 'package:bitsxlamarato_frontend_2025/features/screens/micro/mic.dart';
 import 'package:bitsxlamarato_frontend_2025/features/screens/patient/patient_menu_page.dart';
+import 'package:bitsxlamarato_frontend_2025/features/screens/doctor/doctor_home_page.dart';
 import 'package:bitsxlamarato_frontend_2025/services/api_service.dart';
 import 'package:bitsxlamarato_frontend_2025/services/navigation_service.dart';
 import 'package:bitsxlamarato_frontend_2025/services/session_manager.dart';
@@ -16,13 +17,21 @@ Future<void> main() async {
   );
 
   final startLoggedIn = await ApiService.restoreSession();
-  final userData = await SessionManager.getUserData();
+  Map<String, dynamic>? userData = await SessionManager.getUserData();
+  if (startLoggedIn && (userData == null || userData['user_type'] == null)) {
+    try {
+      await ApiService.getAndCacheCurrentUser();
+      userData = await SessionManager.getUserData();
+    } catch (_) {}
+  }
   final alreadyRespondedToday =
       userData != null && userData['already_responded_today'] == true;
+  final userType = (userData?['user_type'] as String?) ?? 'unknown';
   runApp(
     MyApp(
       startLoggedIn: startLoggedIn,
       startInActivities: alreadyRespondedToday,
+      userType: userType,
     ),
   );
 }
@@ -30,10 +39,12 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   final bool startLoggedIn;
   final bool startInActivities;
+  final String userType;
   const MyApp({
     super.key,
     this.startLoggedIn = false,
     this.startInActivities = false,
+    this.userType = 'unknown',
   });
 
   @override
@@ -47,14 +58,17 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: startLoggedIn
-          ? (startInActivities
-              ? const PatientMenuPage()
-              : const MicScreen())
+          ? (userType == 'doctor'
+              ? const DoctorHomePage()
+              : (startInActivities
+                  ? const PatientMenuPage()
+                  : const MicScreen()))
           : const InitialPage(),
       routes: {
         '/initial': (context) => const InitialPage(),
         '/register': (context) => const RegisterLobby(),
         '/login': (context) => const LoginScreen(),
+        '/doctor': (context) => const DoctorHomePage(),
       }
     );
   }
