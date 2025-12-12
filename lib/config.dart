2bb74c86a8153fc0e94f1e2ea579b4f2cf731efb
Ernest Rull Turigas <ernestrull04@gmail.com>
2025-12-12 22:53:46 +0100

    Update default API URL in Config
    Changed the default API URL in Config from an external address to 'http://loaclhost:5000' for local development.

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static const String apiUrl = String.fromEnvironment('API_URL',
-      defaultValue: 'http://hg209znye8r.sn.mynetname.net:56969');
+  static const String apiUrl =
+      String.fromEnvironment('API_URL', defaultValue: 'http://loaclhost:5000');
 }
